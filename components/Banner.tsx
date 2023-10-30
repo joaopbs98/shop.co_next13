572d94a7213958e40cd5183f52b6ae4cd37a55af
João Pedro Silva <88547615+joaopbs98@users.noreply.github.com>
2023-10-30 10:23:25 +0000

    New arrivals (Possible fix on Vercel) (#5)
    * hero mostly done
    
    * banner update
    
    * update
    
    * Possible error fix

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -17,7 +17,13 @@ const Banner = () => {
   }, []);
 
   return (
-    <div className="w-full bg-black w-screen absolute py-4 left-0 inline-flex flex-nowrap overflow-hidden mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)">
+    <div
+      className="w-full bg-black w-screen absolute py-4 left-0 inline-flex flex-nowrap overflow-hidden"
+      style={{
+        maskImage:
+          "linear-gradient(to right, transparent 0, black 128px, black calc(100% - 128px), transparent 100%)",
+      }}
+    >
       <ul
         id="logos"
         className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
