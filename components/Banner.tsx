944c94ff27f9ca8f981cc0e09542a3c6945050d7
João Pedro Silva <88547615+joaopbs98@users.noreply.github.com>
2023-11-03 15:28:35 +0000

    New arrivals (#9)
    * hero mostly done
    
    * banner update
    
    * update
    
    * Possible error fix
    
    * Banner fix
    
    * Hotfix
    
    * Hotfix #2
    
    * Hotfix #3

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,15 +1,15 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 
 const Banner = () => {
-  const logosRef = React.useRef<HTMLElement | null>(null); // Specify the type as HTMLElement | null
+  const logosRef = useRef<HTMLUListElement | null>(null); // Use RefObject with the correct type
 
   useEffect(() => {
     const logos = logosRef.current;
     if (logos) {
-      const duplicateLogos = logos.cloneNode(true) as HTMLElement; // Cast duplicateLogos to HTMLElement
-      logos.insertAdjacentHTML("afterend", duplicateLogos.outerHTML); // Use insertAdjacentHTML
+      const duplicateLogos = logos.cloneNode(true) as HTMLElement;
+      logos.insertAdjacentHTML("afterend", duplicateLogos.outerHTML);
       duplicateLogos.setAttribute("aria-hidden", "true");
     }
   }, []);
@@ -17,7 +17,7 @@ const Banner = () => {
   return (
     <div className="w-full bg-black w-screen absolute py-4 left-0 inline-flex flex-nowrap overflow-hidden mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)">
       <ul
-        ref={logosRef}
+        ref={logosRef} // Assign the ref to the ul element
         className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
       >
         <li>
